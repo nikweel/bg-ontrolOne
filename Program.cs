ac2a16540fd70c4f445ac8115e22772c68858b30
Николай Федотов <nikweel@nikweel.local>
2022-09-09 19:03:31 +0200

    add task solution

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿//Метод запроса вводных
+string ReadData(string line){
+    Console.WriteLine(line);
+    return Console.ReadLine();
+}
+
+//Метод приведения строки в массив по заданому разделителю и фильтрации не более 3 символов в элементах массива
+string[] СonversionStrToArr(string line, string separator){
+    string[] bufferArray = line.Split(separator);
+    
+    int i = 0;
+    int j = 0;
+    int resultArrayLength = 0;
+    
+    while (i < bufferArray.Length)
+    {
+        if(bufferArray[i].Length <= 3){
+            resultArrayLength++;
+        }
+        i++;
+    }
+    
+    string[] resultArray = new string[resultArrayLength];
+    i = 0;
+    while (i < bufferArray.Length)
+    {
+        if(bufferArray[i].Length <= 3){
+            resultArray[j] = bufferArray[i];
+            j++;
+        }
+        i++;
+    }
+
+    return resultArray;
+}
+
+//Метод печати одномерного массива
+void PrintArray(string[] inputArray)
+{
+    int i = 0;
+    while (i < inputArray.Length - 1)
+    {
+        Console.Write(inputArray[i] + ", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+Console.Clear();
+string inputLine = ReadData("Введите элементы массива реазделяя их запятой");
+PrintArray(СonversionStrToArr(inputLine, ","));
